7798a82c751d52136a3f48a73770c1f484d6f01e
Menno Godeke <mgodeke1994@gmail.com>
2026-05-12 23:19:02 +0200

    comment out i18n for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  #  around_action :switch_locale
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
     { locale: I18n.locale }
   end
 
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
-  end
+  # def switch_locale(&action)
+  #   locale = params[:locale] || I18n.default_locale
+  #   I18n.with_locale(locale, &action)
+  # end
 end
