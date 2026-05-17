dca1d7c95aaf87ccf68ba53a8b93def5b1ff47bb
mennogodeke <mgodeke1994@gmail.com>
2026-05-17 19:24:15 +0200

    Add Dutch and German translations (#22)
    * add static page content
    
    * Add traco gem and start translating model content (#23)
    
    * add traco gem and start translating model content
    
    * fix tests and fixtures

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,7 @@ class Job < ApplicationRecord
   has_many :job_skills, dependent: :destroy
   has_many :skills, through: :job_skills
 
+  translates :title, :description
+
   validates :title, :company, :start_date, presence: true
 end
--- db/migrate/20260507133902_create_jobs.rb
+++ db/migrate/20260507133902_create_jobs.rb
@@ -1,11 +1,17 @@
 class CreateJobs < ActiveRecord::Migration[8.1]
   def change
     create_table :jobs do |t|
-      t.string :title
+      t.string :title_en
+      t.string :title_nl
+      t.string :title_de
       t.string :company
-      t.text :description
+      t.text :description_en
+      t.text :description_nl
+      t.text :description_de
       t.date :start_date
       t.date :end_date
+      t.string :logo
+      t.string :accent
 
       t.timestamps
     end
