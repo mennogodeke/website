ab4c0e25abfe69120ad14ac1f4decb729a167056
Menno Godeke <mgodeke1994@gmail.com>
2026-05-23 00:13:38 +0200

    remove design handoff files

--- design_handoff/career/current_rails_source/career_entry_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  toggle() {
-    const opening = !this.element.classList.contains("open")
-    this.element.classList.toggle("open")
-    if (opening) this.dispatch("opened")
-  }
-}
